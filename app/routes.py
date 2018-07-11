d430dc506056b48a7137aaf44aef80eba3f91cfc
Warren Bain <warren@thoughtcroft.com>
2018-07-11 19:12:52 +1000

    Chapter 2: Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Wazza'}
+    posts = [
+        {
+            'author': {'username': 'Alison'},
+            'body':   'Beautiful day in Sydney!'
+        },
+        {
+            'author': {'username': 'Brian'},
+            'body':   'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Charlie'},
+            'body':   'Brrrrr ... winter is getting colder!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
